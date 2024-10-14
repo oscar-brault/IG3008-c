71d98ce374e736801e8861a7ec0781124a9c0440
oscar-brault <oscar.brault@edu.esiee.fr>
2024-10-14 13:50:58 +0000

    tableaux C

--- tableaux.c
+++ tableaux.c
@@ -1,5 +1,20 @@
 #include <stdlib.h>
 #include <stdio.h>
+int mini(int t[], int size) {
+    if (size == 0) {
+        return -1;  
+    }
+
+    int min = t[0];
+
+    for (int i = 1; i < size; i++) {
+        if (t[i] < min) {
+            min = t[i];
+        }
+    }
+
+    return min;
+}
 int main()
 {
     int t[100];
@@ -8,5 +23,7 @@ int main()
         t[i]=rand() % 50 ;
         printf("t[%d] = %d\n", i, t[i]);
     }
+    int minValue = mini(t, size);
+    printf("La valeur minimale du tableau est : %d\n", minValue);
     return 0;
 }
